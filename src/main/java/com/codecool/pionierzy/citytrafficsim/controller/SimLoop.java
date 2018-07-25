247038a718adb9a6ab84725f9dca7a06a4404cc1
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-07-25 13:54:49 +0200

    vehivleList field, getter and setter added

--- src/main/java/com/codecool/pionierzy/citytrafficsim/controller/SimLoop.java
+++ src/main/java/com/codecool/pionierzy/citytrafficsim/controller/SimLoop.java
@@ -1,11 +1,27 @@
 package com.codecool.pionierzy.citytrafficsim.controller;
 
+import com.codecool.pionierzy.citytrafficsim.model.vehicles.Vehicle;
 import javafx.animation.AnimationTimer;
+
+import java.util.LinkedList;
 
 public class SimLoop extends AnimationTimer {
 
+    private LinkedList<Vehicle> vehicleList = new LinkedList<Vehicle>();
+
     @Override
     public void handle(long now) {
-
+        for (Vehicle v : vehicleList){
+            //v.move();
+        }
     }
+
+    public LinkedList<Vehicle> getVehicleList() {
+        return vehicleList;
+    }
+
+    public void addToVehicleList(Vehicle vehicleToAdd) {
+        this.vehicleList.add(vehicleToAdd);
+    }
+
 }
