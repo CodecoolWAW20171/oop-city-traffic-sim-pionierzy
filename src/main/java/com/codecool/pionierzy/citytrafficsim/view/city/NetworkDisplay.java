f897e542e7d0539b9a9b92a69d3efe347bcd6656
Pico143 <pico19942@gmail.com>
2018-07-26 10:16:20 +0200

    added images for vehicles and loading them into code

--- src/main/java/com/codecool/pionierzy/citytrafficsim/view/city/NetworkDisplay.java
+++ src/main/java/com/codecool/pionierzy/citytrafficsim/view/city/NetworkDisplay.java
@@ -1,6 +1,16 @@
 package com.codecool.pionierzy.citytrafficsim.view.city;
 
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 
+import java.util.HashMap;
+
 public class NetworkDisplay extends Pane {
+    private HashMap<String, Image> images = new HashMap<>();
+
+    private loadImages() {
+        images.put("TRUCK", new Image("truck.jpg"))
+        images.put("CAR", new Image("car.jpg"))
+        images.put("MOTORCYCLE", new Image("motorcycle.jpg"))
+    }
 }
