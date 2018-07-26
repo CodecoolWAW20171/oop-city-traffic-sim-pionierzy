d25cb07a0bca61b912063e41bcedb33abf8558f8
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-07-26 14:28:06 +0200

    improve run() method

--- src/main/java/com/codecool/pionierzy/citytrafficsim/controller/VehicleGenerator.java
+++ src/main/java/com/codecool/pionierzy/citytrafficsim/controller/VehicleGenerator.java
@@ -1,15 +1,29 @@
 package com.codecool.pionierzy.citytrafficsim.controller;
 
+import com.codecool.pionierzy.citytrafficsim.model.city.Edge;
+import com.codecool.pionierzy.citytrafficsim.model.vehicles.Car;
+
+import java.util.ArrayList;
+
 public class VehicleGenerator implements Runnable{
     private final int CAR_INTENSITY = 5;
     private final int TRUCK_INTENSITY = 2;
     private final int MOTORCYCLE_INTENSITY = 2;
-    private final int INTERVAL = 10000; //10 sec
+    private final int INTERVAL = 5000; //5 sec
+    private ArrayList<Edge> startEdges;
+    private SimLoop simLoop;
+
+    public VehicleGenerator(SimLoop simLoop) {
+        this.simLoop = simLoop;
+    }
 
     @Override
     public void run() {
         while (true){
-            //creating vehicle for i<=INTENSITY in each edge
+            for (Edge startEdge : startEdges) {
+                Car car = new Car(startEdge);
+                simLoop.addToVehicleList(car);
+            }
 
             try {
                 Thread.sleep(INTERVAL);
@@ -17,5 +31,9 @@ public class VehicleGenerator implements Runnable{
                 e.printStackTrace();
             }
         }
+    }
+
+    public void setStartEdge(Edge startEdge) {
+        this.startEdges.add(startEdge);
     }
 }
