af472a146d8974e79ab09a8f83ca4ed5253132e1
Pico143 <pico19942@gmail.com>
2018-08-20 10:59:33 +0200

    removed unnecessary edge constructor

--- src/main/java/com/codecool/pionierzy/citytrafficsim/model/city/Edge.java
+++ src/main/java/com/codecool/pionierzy/citytrafficsim/model/city/Edge.java
@@ -15,13 +15,7 @@ public class Edge implements Comparable {
 
 
     private List<Vehicle> vehicles;
-
-    public Edge(NetworkNode beginning, NetworkNode ending) {
-        this.beginning = beginning;
-        this.ending = ending;
-        this.length = Math.sqrt(Math.pow((beginning.getX() - ending.getX()), 2) + Math.pow((beginning.getY() - ending.getY()), 2));
-    }
-
+    
     public Edge(NetworkNode beginning, NetworkNode ending, Pane pane) {
         this.beginning = beginning;
         this.ending = ending;
--- src/test/java/com/codecool/pionierzy/citytrafficsim/model/vehicles/VehicleTest.java
+++ src/test/java/com/codecool/pionierzy/citytrafficsim/model/vehicles/VehicleTest.java
@@ -4,15 +4,17 @@ import com.codecool.pionierzy.citytrafficsim.model.city.Edge;
 import com.codecool.pionierzy.citytrafficsim.model.city.NetworkNode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import javafx.scene.layout.Pane;
 
 class VehicleTest {
+
+    Pane pane = new Pane();
 
     @Test
     void move() {
         NetworkNode node1 = new NetworkNode(5,5);
         NetworkNode node2 = new NetworkNode(100,100);
-        Edge road = new Edge(node1, node2);
+        Edge road = new Edge(node1, node2, pane);
         Car car = new Car(road);
         for (int i = 0; i < 5; i++) {
             car.accelerate();
