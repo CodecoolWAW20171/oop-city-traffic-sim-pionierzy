43459c776681bcfdb57f8c148335afcfcaec404b
Pico143 <pico19942@gmail.com>
2018-08-22 09:57:22 +0200

    added lights status constructor

--- src/main/java/com/codecool/pionierzy/citytrafficsim/model/lights/Lights.java
+++ src/main/java/com/codecool/pionierzy/citytrafficsim/model/lights/Lights.java
@@ -5,4 +5,10 @@ import com.codecool.pionierzy.citytrafficsim.model.city.NetworkNode;
 public class Lights {
     private NetworkNode location;
     private int timeLeft;
+    private LightsStatus lightsStatus;
+
+    public Lights(NetworkNode location) {
+        timeLeft = 10;
+        lightsStatus = LightsStatus.RED;
+    }
 }
